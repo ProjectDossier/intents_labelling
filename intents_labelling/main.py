1a5980b1add171618741d0d8046ce1d34aff2f26
Wojciech Kusa <wojciech.kusa@gmail.com>
2021-10-28 18:50:11 +0200

    feat(labelling): add naive snorkel labelling for transactional queries

--- intents_labelling/main.py
+++ intents_labelling/main.py
@@ -1,8 +1,12 @@
 from data_loaders import load_orcas
+from intents_labelling.labelling_snorkel import SnorkelLabelling
 
 
 if __name__ == "__main__":
     df = load_orcas()
+
+    sl = SnorkelLabelling()
+    df = sl.predict_transactional(df=df)
 
     outfile = "../data/output/orcas-small.tsv"
     df.to_csv(outfile, sep="\t")
