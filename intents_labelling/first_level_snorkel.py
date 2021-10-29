9c5e9009f0f917deb556e0bcbcbc65b0e79c8e50
Wojciech Kusa <wojciech.kusa@gmail.com>
2021-10-29 14:32:04 +0200

    feat(labelling): add navigational labelling function that works for top level domain names

--- intents_labelling/first_level_snorkel.py
+++ intents_labelling/first_level_snorkel.py
@@ -1,11 +1,11 @@
 import pandas as pd
-from snorkel.labeling import labeling_function, PandasLFApplier
+from snorkel.labeling import labeling_function, PandasLFApplier, LFAnalysis
 from snorkel.labeling.model import LabelModel
 
 
 TRANSACTIONAL = 1
-NAVIGATIONAL = -1
-ABSTAIN = 0
+NAVIGATIONAL = 0
+ABSTAIN = -1
 
 
 """TRANSACTIONAL Labelling functions"""
@@ -62,27 +62,46 @@ def lf_www_lookup(x):
     )
 
 
+with open("../data/helpers/top_level_domains.txt") as fp:
+    domain_names_list = [line.strip() for line in fp.readlines()]
+
+
+@labeling_function()
+def lf_domain_name_lookup(x):
+    return (
+        NAVIGATIONAL
+        if any(word in x.query.lower() for word in domain_names_list)
+        else ABSTAIN
+    )
+
+
 class SnorkelLabelling:
     def __init__(self):
-        self.transactional_lfs = [
+        self.lfs = [
             lf_keyword_lookup,
             lf_download_lookup,
             lf_audio_video_lookup,
             lf_extension_lookup,
             lf_transaction_lookup,
             lf_www_lookup,
+            lf_domain_name_lookup,
         ]
 
     def predict_first_level(self, df: pd.DataFrame) -> pd.DataFrame:
-        applier = PandasLFApplier(lfs=self.transactional_lfs)
+        applier = PandasLFApplier(lfs=self.lfs)
         L_train = applier.apply(df=df)
 
         label_model = LabelModel(cardinality=2, verbose=True)
         label_model.fit(L_train=L_train, n_epochs=500, log_freq=100, seed=123)
+
+        print(LFAnalysis(L=L_train, lfs=self.lfs).lf_summary())
 
         df["Labels"] = label_model.predict(L=L_train, tie_break_policy="abstain")
 
         df.loc[df["Labels"] == TRANSACTIONAL, "Labels"] = "Transactional"
         df.loc[df["Labels"] == NAVIGATIONAL, "Labels"] = "Navigational"
         df.loc[df["Labels"] == ABSTAIN, "Labels"] = "Abstain"
+
+        print(df["Labels"].value_counts())
+
         return df
