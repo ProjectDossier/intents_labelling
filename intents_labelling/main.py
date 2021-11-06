716a7475bd0931490238f4f992d8f3310ede4448
Wojciech Kusa <wojciech.kusa@gmail.com>
2021-11-06 14:30:53 +0100

    feat(labelling): add scripts for labelling first and second level intents with snorkel

--- intents_labelling/main.py
+++ intents_labelling/main.py
@@ -1,12 +1,12 @@
 from data_loaders import load_orcas
-from intents_labelling.first_level_snorkel import SnorkelLabelling
-
+from intents_labelling.snorkel_labelling import SnorkelLabelling
 
 if __name__ == "__main__":
     df = load_orcas()
 
     sl = SnorkelLabelling()
     df = sl.predict_first_level(df=df)
+    df = sl.predict_second_level(df=df)
 
-    outfile = "../data/output/orcas_small.tsv"
+    outfile = f"../data/output/orcas_{len(df)}.tsv"
     df.to_csv(outfile, sep="\t")
