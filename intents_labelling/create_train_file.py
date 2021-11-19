b5019eda7953ae1180daffe8d957be2490195c4a
Wojciech Kusa <wojciech.kusa@gmail.com>
2021-11-19 18:03:06 +0100

    feat: add script for sampling train data and filtering out testset entries

---
+++ intents_labelling/create_train_file.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+from intents_labelling.data_loaders import (
+    load_labelled_orcas,
+    load_unlabelled_orcas,
+    save_orcas,
+)
+
+
+def prepare_train_set(
+    orcas_df: pd.DataFrame, test_df: pd.DataFrame, train_size: int
+) -> pd.DataFrame:
+
+    test_df["test"] = True
+    merged_df = pd.merge(orcas_df, test_df, how="outer")
+    df = orcas_df[orcas_df.index.isin(merged_df[merged_df["test"] != True].index)]
+
+    df = df.sample(train_size, random_state=42)
+
+    return df
+
+
+if __name__ == "__main__":
+    testset_location = "data/test/orcas_test.tsv"
+    raw_orcas_location = "data/input/orcas.tsv"
+
+    train_size = 1000000
+    trainset_location = f"data/input/orcas_train_{train_size}.tsv"
+
+    test_df = load_labelled_orcas(data_path=testset_location)
+    orcas_df = load_unlabelled_orcas(data_path=raw_orcas_location)
+
+    train_df = prepare_train_set(
+        orcas_df=orcas_df, test_df=test_df, train_size=train_size
+    )
+
+    save_orcas(df=train_df, outfile=trainset_location)
