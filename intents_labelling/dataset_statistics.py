548aa82d2dfafb83f2a82e5be63aa979be1252bd
Wojciech Kusa <wojciech.kusa@gmail.com>
2021-12-14 11:30:14 +0100

    feat(data): add script for simple dataset statistics

---
+++ intents_labelling/dataset_statistics.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import Levenshtein as lev
+
+
+def calculate_statistics(df: pd.DataFrame, label_column: str):
+    print(f"size of the dataset: {len(df)} examples")
+    print(f"unique queries: {len(df['query'].unique())}")
+    print(f"unique urls: {len(df['url'].unique())}")
+    print(f"unique query_url pairs: {len(df[['query', 'url']].value_counts())}")
+
+    print("\n")
+    print("label distribution:")
+    print(100 * df[label_column].value_counts() / len(df))
+
+    print("\n")
+    lev_list = []
+    for url, query in zip(df["url"].tolist(), df["query"].tolist()):
+        lev_list.append(lev.distance(url, query))
+    df["levenshtein"] = lev_list
+    print("Mean Levenshtein distance between query and url:")
+    print(df.groupby(label_column)["levenshtein"].mean())
+
+    print("\n")
+    print("queries with the same url but different labels:")
+    for url in df["url"].value_counts()[df["url"].value_counts() > 1].index.tolist():
+        if len(df.loc[df["url"] == url, label_column].unique()) > 1:
+            print(df.loc[df["url"] == url])
+
+
+if __name__ == "__main__":
+    dataset_path = "data/test/orcas_test.tsv"
+    df = pd.read_csv(dataset_path, sep="\t")
+    calculate_statistics(df=df, label_column="label_manual")
