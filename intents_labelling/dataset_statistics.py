4447fd4a5e4fba2b37d33ad860ef06fd92178efd
Wojciech Kusa <wojciech.kusa@gmail.com>
2021-12-14 11:33:26 +0100

    fix(data): small fix to dataset statistics

--- intents_labelling/dataset_statistics.py
+++ intents_labelling/dataset_statistics.py
@@ -2,7 +2,9 @@ import pandas as pd
 import Levenshtein as lev
 
 
-def calculate_statistics(df: pd.DataFrame, label_column: str):
+def calculate_statistics(
+    df: pd.DataFrame, label_column: str, url_label_mismatch: bool = False
+):
     print(f"size of the dataset: {len(df)} examples")
     print(f"unique queries: {len(df['query'].unique())}")
     print(f"unique urls: {len(df['url'].unique())}")
@@ -12,19 +14,22 @@ def calculate_statistics(df: pd.DataFrame, label_column: str):
     print("label distribution:")
     print(100 * df[label_column].value_counts() / len(df))
 
-    print("\n")
     lev_list = []
     for url, query in zip(df["url"].tolist(), df["query"].tolist()):
         lev_list.append(lev.distance(url, query))
     df["levenshtein"] = lev_list
+    print("\n")
     print("Mean Levenshtein distance between query and url:")
     print(df.groupby(label_column)["levenshtein"].mean())
 
-    print("\n")
-    print("queries with the same url but different labels:")
-    for url in df["url"].value_counts()[df["url"].value_counts() > 1].index.tolist():
-        if len(df.loc[df["url"] == url, label_column].unique()) > 1:
-            print(df.loc[df["url"] == url])
+    if url_label_mismatch:
+        print("\n")
+        print("queries with the same url but different labels:")
+        for url in (
+            df["url"].value_counts()[df["url"].value_counts() > 1].index.tolist()
+        ):
+            if len(df.loc[df["url"] == url, label_column].unique()) > 1:
+                print(df.loc[df["url"] == url])
 
 
 if __name__ == "__main__":
